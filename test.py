2dcb2f28bf8afb7e45c95e33ff3be55227b575b0
Junru Shao <junrushao1994@gmail.com>
2021-10-09 02:44:44 -0700

    done

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
+import ctypes
 import numpy as np
 import tvm
-from dlpack import from_numpy
+from dlpack import from_numpy, to_numpy
 
 
 def test_from_numpy():
-    # Test converting numpy to tvm ndarray
-    np_array = np.arange(10, dtype="int32").reshape((2, 5))
+    np_array = np.random.normal(size=[10, 10])
     np_array_ref = np_array.copy()
     tvm_array = from_numpy(np_array, tvm.nd.from_dlpack)
     del np_array
@@ -13,14 +13,12 @@ def test_from_numpy():
     del tvm_array
 
 
-def test_nd_to_np():
-    # Test converting tvm ndarray to numpy
-    print("### Testing nd_to_np")
-    array = tvm.nd.array(np.random.normal(size=[10, 10]))
-    array_ref = array.numpy()
-    np_array = nd_to_np(array)
-    del array
-    np.testing.assert_equal(actual=array_ref, desired=np_array)
+def test_to_numpy():
+    tvm_array = tvm.nd.array(np.random.normal(size=[10, 10]))
+    np_array_ref = tvm_array.numpy()
+    np_array = to_numpy(tvm_array)
+    del tvm_array
+    np.testing.assert_equal(actual=np_array, desired=np_array_ref)
     del np_array
 
 
@@ -28,4 +26,6 @@ if __name__ == "__main__":
     print("### Testing from_numpy")
     for i in range(10000):
         test_from_numpy()
-    #test_nd_to_np()
+    print("### Testing to_numpy")
+    for i in range(10000):
+        test_to_numpy()
